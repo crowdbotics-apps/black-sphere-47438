a17b9924b02dbe236502d031886c4323edce2518
Crowdbotics <team@crowdbotics.com>
2024-04-10 02:05:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-sphere-47438",
+  url: "https://black-sphere-47438.botics.co",
+  api: "https://black-sphere-47438.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
